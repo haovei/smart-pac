a797fa2ab0bdecf5d5a5f312406c483f3175fe24
Henry <haovei@gmail.com>
2024-03-04 19:02:06 +0800

    fix: dir

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,3 @@
 import path from 'path';
 
-export const CONFIG_FILE =
-  process.env.CONFIG_FILE ?? path.join(global.__dirname, '../config/host.json');
+export const CONFIG_FILE = process.env.CONFIG_FILE ?? path.join(import.meta.dir, '../config/host.json');
