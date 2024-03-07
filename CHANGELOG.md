5f0d36e261b5c46fd0b0e4d3c35374973a5966c8
Henry <haovei@gmail.com>
2024-03-07 20:51:46 +0800

    chore: changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+# [1.0.0](https://github.com/haovei/smart-pac/compare/v0.2.0...v1.0.0) (2024-03-07)
--- src/const.ts
+++ src/const.ts
@@ -6,4 +6,4 @@ const __dirname = dirname(__filename);
 
 export const CONFIG_FILE = process.env.CONFIG_FILE ?? join(__dirname, '../config/host.json');
 export const ACCESS_TOKEN = process.env.ACCESS_TOKEN ?? '';
-export const PORT = process.env.PORT ?? 8080;
+export const PORT = process.env.PORT ?? 3000;
